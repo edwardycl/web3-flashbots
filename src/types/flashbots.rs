e96e8d43f97cbed82952be2d709333e48fb6e08b
Youngchan Lee <youngchan@hyperithm.com>
2021-05-11 14:11:18 +0900

    Make fields of flashbots types public

--- src/types/flashbots.rs
+++ src/types/flashbots.rs
@@ -5,29 +5,42 @@ use serde::{Deserialize, Deserializer};
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, Debug, Deserialize)]
 pub struct CallBundleResponse {
+    /// Bundle gas price
     #[serde(deserialize_with = "u256_from_dec_str")]
-    bundle_gas_price: U256,
-    bundle_hash: H256,
+    pub bundle_gas_price: U256,
+    /// Bundle hash
+    pub bundle_hash: H256,
+    /// Coinbase diff (gas fees + eth sent to coinbase)
     #[serde(deserialize_with = "u256_from_dec_str")]
-    coinbase_diff: U256,
+    pub coinbase_diff: U256,
+    /// Eth sent to coinbase
     #[serde(deserialize_with = "u256_from_dec_str")]
-    eth_sent_to_coinbase: U256,
+    pub eth_sent_to_coinbase: U256,
+    /// Gas fees in eth
     #[serde(deserialize_with = "u256_from_dec_str")]
-    gas_fees: U256,
-    results: Vec<TransactionSimulation>,
-    state_block_number: u64,
-    total_gas_used: u64,
+    pub gas_fees: U256,
+    /// Transaction simulation results
+    pub results: Vec<TransactionSimulation>,
+    /// State block number used for simulation
+    pub state_block_number: u64,
+    /// Total gas used
+    pub total_gas_used: u64,
 }
 
 /// Transaction simulation result
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, Debug, Deserialize)]
 pub struct TransactionSimulation {
-    gas_used: u64,
-    tx_hash: H256,
-    value: Option<Bytes>,
-    error: Option<String>,
-    revert: Option<String>, // TODO: parse encoded string
+    /// Gas used
+    pub gas_used: u64,
+    /// Transaction hash
+    pub tx_hash: H256,
+    /// Value
+    pub value: Option<Bytes>,
+    /// Error message (e.g. "execution reverted")
+    pub error: Option<String>,
+    /// Revert message
+    pub revert: Option<String>, // TODO: parse encoded string
 }
 
 fn u256_from_dec_str<'de, D>(de: D) -> Result<U256, D::Error>
